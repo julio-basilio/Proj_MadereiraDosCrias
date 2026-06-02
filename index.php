7b7dde117aae626d5489b6e1aa0179b7aefcd74e
julio-basilio <juliobasilio780@gmail.com>
2026-06-02 16:35:19 -0300

    Colocando a memoria do Billy Bruto só pra ter algo

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Madereira dos Crias</title>
+    <link rel="stylesheet" href="Css/style.css">
 </head>
 <body>
-    
+    <img src="Img/images.jpeg" alt="" height=100>
+    <p id="paragrafo">Em mémoria de: Billy Bruto</p>
+    <p id="memoria" >★ 7/06/1972 - ✟ 20/05/2026</p>
+    <p id="frase">Veja, as pessoas adoram aquela sensação aconchegante que os supers lhes proporcionam.<br> Um pouco de ouro para voar do céu e salvar o dia, para que você não precise fazer isso <br> sozinho. Mas se você soubesse metade da merda que eles fazem? Ooh... diabólico pra <br>caralho. Mas então... é aí que eu entro.</p>
+    <p id="aspas1">“</p>
+    <p id="aspas2">„</p>
 </body>
 </html>
