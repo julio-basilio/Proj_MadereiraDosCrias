d0b3a8b9b09392e501ba1eda1ea43883290efe21
Clona-Cartão_2.0 <juliobasilio780@gmail.com>
2026-06-02 22:25:11 -0300

    Brinks

---
+++ cap.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Capitão</title>
+    <link rel="stylesheet" href="Css/cap.css">
+</head>
+<body>
+
+    <main class="capitao-container">
+        <div class="moldura-imagem">
+            <img src="Img/Homelander_29.webp" alt="Capitão Pátria">
+        </div>
+        <h1>OLÁ!!</h1>
+    </main>
+    
+</body>
+</html>
