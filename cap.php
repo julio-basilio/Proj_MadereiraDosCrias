bcf0cab21694ec6a66435a06da43ee970e745e49
Clona-Cartão_2.0 <juliobasilio780@gmail.com>
2026-06-02 22:45:59 -0300

    Brinks

--- cap.php
+++ cap.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Capitão</title>
+    <title>Capitão Pátria - Vought International</title>
     <link rel="stylesheet" href="Css/cap.css">
 </head>
 <body>
 
     <main class="capitao-container">
         <div class="moldura-imagem">
-            <img src="Img/Homelander_29.webp" alt="Capitão Pátria">
+            <img src="Img/Homelander_29.webp" alt="Retrato oficial do Capitão Pátria em seu uniforme azul e capa vermelha">
         </div>
+        
         <h1>OLÁ!!</h1>
     </main>
     
