3d638d5ab71048c744f6d290811e6d3081ba1c83
Clona-Cartão_2.0 <juliobasilio780@gmail.com>
2026-06-20 23:08:51 -0300

    coloquei foto e nome do usuario em todas as paginas

--- Js/entrega.js
+++ Js/entrega.js
@@ -76,3 +76,19 @@ async function buscarCEP(){
     }
 
 }
+
+const nomeTela = localStorage.getItem('nomeUsuario');
+const fotoperfil = localStorage.getItem('urlImagem67');
+
+const divNome = document.getElementById('nomeUsuario');
+const divPerfil = document.getElementById('fotoPerfil');
+
+if (divNome && nomeTela) {
+    divNome.innerHTML = `<span>${nomeTela.toUpperCase()}</span>`;
+}
+
+if (divPerfil && fotoperfil) {
+    divPerfil.innerHTML = `<img src="${fotoperfil}" alt="Foto de Perfil" style="width: 40px; height: 40px; border-radius: 50%; object-fit: cover;" />`;
+}
+
+console.log("foto perfil: " + fotoperfil);
