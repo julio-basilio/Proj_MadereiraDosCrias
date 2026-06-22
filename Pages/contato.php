2f534300783fd1da49a2a74807a3ff257cec34d1
david889 <davimoreiraa92@gmail.com>
2026-06-22 10:21:31 -0300

    davi22/06 contato

--- Pages/contato.php
+++ Pages/contato.php
@@ -1,22 +1,22 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Madeireira Nobre Prime - Excelência em Madeiras Nobres</title>
-    <link rel="stylesheet" href="../Css/molde.css">
-    <link rel="stylesheet" href="../Css/contato.css">
+    <title>Madeireira Sahur</title>
     
-    <link rel="icon" type="image/x-icon" href="../Img/Logo Triple T.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    
+    <link rel="icon" type="image/x-icon" href="../Img/Logo Triple T.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <link rel="stylesheet" href="../Css/contato.css">
     <link rel="stylesheet" href="../Css/dropdown.css">
-
 </head>
 <body>
 
@@ -24,62 +24,100 @@
     
         <header id="header">
 
-            <h2>FRETE GRATIS PARA TODO BRASIL!</h2>
+            <div class="header-top">
+
+                <a href="contato.php" class="left-link">
+                    WhatsApp: (99) 9 9999-9999
+                </a>
+
+                <span class="frete">
+                    FRETE GRÁTIS PARA TODO BRASIL!
+                </span>
+
+                <a href="contato.php" class="right-link">
+                    Entrar em contato
+                </a>
+
+            </div>
+           
 
             <nav>
+
                 <div class="logo">
 
-                    <img src="../Img/logosahurmadeireira.png" height="120vh" width="120vh" alt="">
-
+                    <a href="index.php">
+                        <img src="../Img/logosahurmadeireira.png" height="120vh" width="120vh" alt="Logo">
+                    </a>
                     <h1 class="logo-text">
                         MADEIREIRA <br> <span class="logo-span">SAHUR</span>
                     </h1>
                 </div>
                 
-                <div class="search-box">
-                    <input type="text" placeholder=" O que você procura?">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </div>
-                
+                <form action="busca.php" method="GET" class="search-box">
+                   <input type="text" name="q" placeholder="O que você procura?" required>
+
+                    <button type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+               
                 <div class="nav-icons">
 
-                    <button class="icon-btn" id="btnCategorias" title="Categorias" style="position: relative;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
-                        </svg>
-                        <span>Categorias</span>
-                    </button>
+                    <a href="categorias.php">
+                        <button class="icon-btn" id="catBtn" title="Categorias" style="position: relative;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-list-ul" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2m0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                            </svg>
+                            <span>Categorias</span>
+                        </button>
+                    </a>
 
-                    <button class="icon-btn" id="btnEntrega" title="Entrega" style="position: relative;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
-                        <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
-                        </svg>
-                        <span>Entrega</span>
-                    </button>
+                    <a href="calculofrete.php">
+                        <button class="icon-btn" id="deliveryBtn" title="Entrega" style="position: relative;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                                <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
+                            </svg>
+                            <span>Entrega</span>
+                        </button>
+                    </a>
 
-                     <div onclick="sair()" id="usuario-container">
+                    <a href="carrinho.php">
+                        <button class="icon-btn" id="cartBtn" title="Carrinho" style="position: relative;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                            </svg>
+                            <span>Carrinho</span>
+                            <span class="cart-badge">
+                              <?php
+
+                                if(isset($_SESSION['carrinho']))
+                                {
+                                    echo count($_SESSION['carrinho']);
+                                }
+                                else
+                                {
+                                    echo 0;
+                                }
+                                ?>
+                            </span>
+                        </button>
+                    </a>
+
+                      <div onclick="sair()" id="usuario-container">
                         <div id="fotoPerfil"></div>
                         <div id="nomeUsuario"></div>
                     </div>
-
-                    <button class="icon-btn" id="btnCarrinho" title="Carrinho" style="position: relative;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-                        </svg>
-                        <span>Carrinho</span>
-                        <span class="cart-badge">0</span>
-                    </button>
+                    
                 </div>
-
             </nav>
 
             <ul class="nav-categorias">
-                <li><a href="">Madeiras Brutas</a></li>
-                <li><a href="">Madeiras Finas</a></li>
-                <li><a href="">MDF</a></li>
-                <li><a href="">Portas e Janelas</a></li>
-                <li><a href="">Ferragens</a></li>
-                <li><a href="">Ferramentas</a></li>
+                <li><a href="madeiras_brutas.php">Madeiras Brutas</a></li>
+                <li><a href="madeiras-finas.php">Madeiras Finas</a></li>
+                <li><a href="mdf.php">MDF</a></li>
+                <li><a href="portas-janelas.php">Portas e Janelas</a></li>
+                <li><a href="ferragens.php">Ferragens</a></li>
+                <li><a href="ferramentas.php">Ferramentas</a></li>
 
                  <li class="opcao">
                     <a href="">Páginas</a>
@@ -110,26 +148,29 @@
 
         </header>
         <main >
+
             <div class="formTamanho">
-            <form action="envio.php" method="POST">
-                <label for="nome">Nome: </label>
-                <input type="text" name ="nome"id="nome"><br>
 
-                <label for="email">E-mail: </label>
-                <input type="email" name ="email" id="email"><br>
+                <form action="envio.php" method="POST">
+                    <label for="nome">Nome: </label>
+                    <input type="text" name ="nome"id="nome"><br>
 
-                <label for="mensagem">Mensagem: </label><br>
-                <textarea id="mensagem" name="mensagem" rows="4" required class="tamanhobox"></textarea><br><br>
+                    <label for="email">E-mail: </label>
+                    <input type="email" name ="email" id="email"><br>
 
-                <button type="submit">Enviar</button>
-            </form>
+                    <label for="mensagem">Mensagem: </label><br>
+                    <textarea id="mensagem" name="mensagem" rows="4" required class="tamanhobox"></textarea><br><br>
+
+                    <button type="submit" class="btn" >Enviar</button>
+                </form>
+
             </div>
+
         </main>
 
-                  <footer class="footer">
+                <footer class="footer">
 
             <section class="footer-top">
-
                 <img src="../Img/sahurFooter.png" alt="">
                 
                 <div class="social-area">
@@ -146,18 +187,17 @@
             <section class="footer-content">
                 <div class="footer-column">
                     <h4>Navegação</h4>
-                    <a href="#">Início</a>
-                    <a href="#">Produtos</a>
-                    <a href="#">Categorias</a>
-                    <a href="#">Contato</a>
+                    <a href="index.php">Início</a>
+                    <a href="mdf.php">Produtos</a>
+                    <a href="Categorias.php">Categorias</a>
+                    <a href="contato.php">Contato</a>
                 </div>
 
                 <div class="footer-column">
                     <h4>Categorias</h4>
-                    <a href="#">Madeiras Brutas</a>
-                    <a href="#">Madeiras Finas</a>
-                    <a href="#">MDF</a>
-                    <a href="#">Compensados</a>
+                    <a href="madeiras_brutas.php">Madeiras Brutas</a>
+                    <a href="madeiras-finas.php">Madeiras Finas</a>
+                    <a href="mdf.php">MDF</a>
                 </div>
 
                 <div class="footer-column">
@@ -166,7 +206,7 @@
                         Este site é fictício e foi desenvolvido
                         apenas para fins de estudo e portfólio.
                     </p>
-                    <a href="https://github.com/seuusuario/seurepositorio" target="_blank" class="repo-link">Ver Repositório no GitHub</a>
+                    <a href="https://github.com/julio-basilio/Proj_MadereiraDosCrias.git" target="_blank" class="repo-link">Ver Repositório no GitHub</a>
                 </div>
             </section>
 
@@ -179,7 +219,8 @@
 
     </div>
     
+ <!-- JAVASCRIPT -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="../Js/script.js"></script>
+    <script src="../Js/index.js"></script>
 </body>
 </html>
